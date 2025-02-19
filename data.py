1fda4cec4649775c7064b341e7e8a98fb8cadd9b
Sebastian G. Rivera <sguarneros91@icloud.com>
2025-02-19 01:00:16 -0600

    Se modifica url

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-a6890648-fe1f-4e0a-b204-5cc06c1fd156.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-66ed294a-37bc-4986-97d6-e803ccc57846.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
